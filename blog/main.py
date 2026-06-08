3e10bd7c96916a2a3b21c716c9059820c0ab7c08
Rajveer Singh Tanwar <rajveersinghtanwar28@gmail.com>
2026-06-09 02:03:12 +0530

    message for the home page added

--- blog/main.py
+++ blog/main.py
@@ -15,3 +15,7 @@ models.base.metadata.create_all(engine)
 app.include_router(blog.router)
 app.include_router(user.router)
 app.include_router(authentication.router)
+
+@app.get('/')
+def home():
+    return {'message': 'Welcome to the Blog API!'}
